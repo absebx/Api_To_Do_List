48b2e45faee0b420503e5be2a4a36fa2b627a371
asder707 <abn.sebick@gmail.com>
2017-04-09 13:32:31 -0300

    Se modifica configuracion de api para que retorne json

--- Api/App_Start/WebApiConfig.cs
+++ Api/App_Start/WebApiConfig.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using System.Net.Http.Headers;
 
 namespace Api
 {
@@ -21,6 +22,10 @@ namespace Api
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+            //configuracion para que retorne json
+            config.Formatters.JsonFormatter.SupportedMediaTypes
+                .Add(new MediaTypeHeaderValue("text/html"));
         }
     }
 }

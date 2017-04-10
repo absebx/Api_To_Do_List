857e54448ddb096f03db7b7cb1c09902814f1859
asder707 <abn.sebick@gmail.com>
2017-04-10 13:18:16 -0300

    Se crea clase Board en modelo

---
+++ Modelo/Board.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo
+{
+    public class Board
+    {
+        public long Id { get; set; }
+        public long Name { get; set; }
+        public long User_id { get; set; }
+    }
+}

f551e8a614b4fabda100c7ad1166c997ebdb413f
asder707 <abn.sebick@gmail.com>
2017-04-10 14:40:36 -0300

    Se crea modelo Ticket en modelo

---
+++ Modelo/Ticket.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo
+{
+    public class Ticket
+    {
+        public long Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+        public int EstimatedTime { get; set; }
+        public long BoardID { get; set; }
+    }
+}
